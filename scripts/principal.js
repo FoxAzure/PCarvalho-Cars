c5ba9812437bf69ebb719395ed46078ea45663ed
FoxAzure <108664801+FoxAzure@users.noreply.github.com>
2023-01-03 11:37:02 -0300

    Add files via upload

--- scripts/principal.js
+++ scripts/principal.js
@@ -39,30 +39,82 @@ function pesquisarTabela(){
 
 
 var carros_lista = [
-    {nome: 'fiat-strada', titulo: "Fiat_Strada", foto: 'galery/carros/Fiat-Strada-001.jpg'},
-    {nome: 'hyundai-hb20', titulo: "Hyundai_HB20", foto: 'galery/carros/hyundai-hb20-001.jpg'}
+    {
+        id:         01,
+        nome:       'Strada',
+        marca:      'Fiat',
+        category:   'Picape compacta',
+        descr:      'É a picape pequena de maior sucesso no Brasil e a líder no segmento. Introduziu novidades na categoria como a opção de cabine dupla',
+        foto:       'galery/carros/fiat_strada_2023_1_29072022_71924_1280_960.jpg',
+        galery1:    'galery/carros/fiat_strada_2023_1_16082022_72018_1280_960.jpg',
+        galery2:    'galery/carros/fiat_strada_2023_1_29072022_71922_1280_960.jpg',
+        galery3:    'galery/carros/fiat_strada_2023_1_29072022_71923_1280_960.jpg',
+        galery4:    'galery/carros/fiat_strada_2023_1_29072022_71926_1280_960.jpg',
+    },
+
+    {
+        id:         02,
+        nome:       'HB20',
+        marca:      'Hyundai',
+        category:   'Hatch compacto',
+        descr:      'Primeiro modelo nacional desenvolvido pela Hyundai para o Brasil. Fabricado em Piracicaba desde 2012.',
+        foto:       'galery/carros/hyundai_hb20_2023_1_02072022_71753_1280_960.jpg',
+        galery1:    'galery/carros/hyundai_hb20_2023_1_02072022_71752_1280_960.jpg',
+        galery2:    'galery/carros/hyundai_hb20_2021_1_11072020_31392_1280_960.jpg',
+        galery3:    'galery/carros/hyundai_hb20_2023_1_06072022_71771_1280_960.jpg',
+        galery4:    'galery/carros/hyundai_hb20_2023_1_06072022_71772_1280_960.jpg',
+    },
+
+    {
+        id:         03,
+        nome:       'Onix',
+        marca:      'Chevrolet',
+        category:   'Hatch compacto',
+        descr:      'É produzido em Gravataí desde 2012 e em 2015 passou a ser o carro mais vendido do Brasil.',
+        foto:       'galery/carros/chevrolet_onix_2021_1_24022022_70721_1280_960.jpg',
+        galery1:    'galery/carros/chevrolet_onix_2021_1_14022021_46195_1280_960.jpg',
+        galery2:    'galery/carros/chevrolet_onix_2021_1_14022021_46197_1280_960.jpg',
+        galery3:    'galery/carros/chevrolet_onix_2020_1_06092019_15700_1280_960.jpg',
+        galery4:    'galery/carros/chevrolet_onix_2021_1_23092020_34062_1280_960.jpg',
+    },
+
 ]
 
 
-function selectCar(carro){
+function selectCar(carro_id){
 
-    var i, bg_car, nome_carro;
+    var i, bg_car, lista_id, nome_carro;
 
     for(i=0;i<carros_lista.length;i++){
 
         
-        nome_carro = carros_lista[i].nome;
+        lista_id = carros_lista[i].id;
 
-        if(nome_carro == carro){
-            bg_car = carros_lista[i].foto;
-            titulo = carros_lista[i].titulo;
+        if(lista_id == carro_id){
+            bg_car      = carros_lista[i].foto;
+            nome_carro  = carros_lista[i].nome;
+            marca_carro  = carros_lista[i].marca;
+            category_carro  = carros_lista[i].category;
+            descr_carro  = carros_lista[i].descr;
+            img01       = carros_lista[i].galery1;
+            img02       = carros_lista[i].galery2;
+            img03       = carros_lista[i].galery3;
+            img04       = carros_lista[i].galery4;
         }
     }
 
     
 
-    document.getElementById('carro-select').style.backgroundImage = 'url('+bg_car+')';
-    document.getElementById('nome-carro').innerHTML = titulo;
+    document.getElementById('tela').style.backgroundImage = 'url('+bg_car+')';
+    document.getElementById('nome-carro').innerHTML = nome_carro;
+    document.getElementById('foto1').style.backgroundImage = 'url('+img01+')';
+    document.getElementById('foto2').style.backgroundImage = 'url('+img02+')';
+    document.getElementById('foto3').style.backgroundImage = 'url('+img03+')';
+    document.getElementById('foto4').style.backgroundImage = 'url('+img04+')';
+    document.getElementById('nome').innerHTML = nome_carro;
+    document.getElementById('marca').innerHTML = marca_carro;
+    document.getElementById('categoria').innerHTML = category_carro;
+    document.getElementById('descricao').innerHTML = descr_carro;
 
 }
 
